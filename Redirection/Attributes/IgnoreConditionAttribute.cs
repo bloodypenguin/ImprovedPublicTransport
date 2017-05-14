0d949b05b8ec5c075e09d67765547b46b1926dbc
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2017-05-14 17:27:14 +0200

    Add ignore attribute to redirection framework

---
+++ Redirection/Attributes/IgnoreConditionAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+
+namespace ImprovedPublicTransport.Redirection.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public abstract class IgnoreConditionAttribute : Attribute
+    {
+        public abstract bool IsIgnored(MethodInfo methodInfo);
+    }
+}
