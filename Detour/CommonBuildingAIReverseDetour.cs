22243b67a56f1703a05199130ead18072f2c5c12
Egor Aralov <aei.omsk@gmail.com>
2017-05-17 00:17:06 +0200

    Fix removing vehicles from line. Only allow depot to serve its primary line type

---
+++ Detour/CommonBuildingAIReverseDetour.cs
@@ -0,0 +1,15 @@
+﻿using ImprovedPublicTransport.Redirection.Attributes;
+
+namespace ImprovedPublicTransport.Detour
+{
+    [TargetType(typeof(CommonBuildingAI))]
+    public class CommonBuildingAIReverseDetour : ShelterAI
+    {
+        [RedirectReverse]
+        public static void CalculateOwnVehicles(CommonBuildingAI ai, ushort buildingID, ref Building data,
+            TransferManager.TransferReason material, ref int count, ref int cargo, ref int capacity, ref int outside)
+        {
+            UnityEngine.Debug.Log("1");
+        }
+    }
+}
