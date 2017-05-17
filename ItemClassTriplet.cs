d271f6f50c1b6002aad3eb2e24063db659cd637a
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2017-05-17 02:45:51 +0200

    Properly get and set values in VehicleEditor

---
+++ ItemClassTriplet.cs
@@ -0,0 +1,18 @@
+﻿namespace ImprovedPublicTransport
+{
+    public struct ItemClassTriplet
+    {
+
+        public ItemClassTriplet(ItemClass.Service service, ItemClass.SubService subService, ItemClass.Level level)
+        {
+            this.Service = service;
+            this.SubService = subService;
+            this.Level = level;
+        }
+
+
+        public ItemClass.Service Service { get; }
+        public ItemClass.SubService SubService { get; }
+        public ItemClass.Level Level { get; }
+    }
+}
