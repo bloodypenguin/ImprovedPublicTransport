3a9ec0a80871ecc284bbd5c087429bd41604de80
Egor Aralov <aei.omsk@gmail.com>
2017-05-21 00:03:22 +0200

    Allow to provide custom format for storing language files

--- TranslationFramework/Language.cs
+++ TranslationFramework/Language.cs
@@ -1,54 +1,13 @@
 ﻿using System.Collections.Generic;
-using System.Xml.Serialization;
 
 namespace ImprovedPublicTransport2.TranslationFramework
 {
-    [XmlRoot(ElementName = "Language", Namespace = "", IsNullable = false)]
-    public class Language
+    public interface ILanguage
     {
-        [XmlAttribute("UniqueName")]
-        public string _uniqueName = "";
+        bool HasTranslation(string id);
 
-        [XmlAttribute("ReadableName")]
-        public string _readableName = "";
+        string GetTranslation(string id);
 
-        [XmlArray("Translations", IsNullable = false)]
-        [XmlArrayItem("Translation", IsNullable = false)]
-        public LanguageConversion[] _conversionGroups
-        {
-            get
-            {
-                LanguageConversion[] returnArray = new LanguageConversion[_conversionDictionary.Count];
-
-                int index = 0;
-                foreach (KeyValuePair<string, string> conversion in _conversionDictionary)
-                {
-                    returnArray[index] = new LanguageConversion() { _key = conversion.Key, _value = conversion.Value };
-                    ++index;
-                }
-
-                return returnArray;
-            }
-
-            set
-            {
-                foreach (LanguageConversion conversion in value)
-                {
-                    _conversionDictionary[conversion._key] = conversion._value;
-                }
-            }
-        }
-
-        [XmlIgnore]
-        public Dictionary<string, string> _conversionDictionary = new Dictionary<string, string>();
-    }
-
-    public class LanguageConversion
-    {
-        [XmlAttribute("ID")]
-        public string _key = "";
-
-        [XmlAttribute("String")]
-        public string _value = "";
+        string LocaleName();
     }
 }
---
+++ TranslationFramework/LanguageDeserializer.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace ImprovedPublicTransport2.TranslationFramework
+{
+    public interface ILanguageDeserializer
+    {
+        ILanguage DeserialiseLanguage(string fileName, TextReader reader);
+    }
+}
