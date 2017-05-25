631aec5e3fac02948932c8f57adc31d02353d5ab
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2017-05-25 21:26:38 +0200

    Update DepotAIMod to 1.7 code

--- Detour/DepotAIMod.cs
+++ Detour/DepotAIMod.cs
@@ -2,7 +2,6 @@
 using ColossalFramework;
 using ImprovedPublicTransport2.RedirectionFramework.Attributes;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 namespace ImprovedPublicTransport2.Detour
 {
@@ -10,36 +9,54 @@ namespace ImprovedPublicTransport2.Detour
     public class DepotAIMod : DepotAI
     {
 
-        //TODO(earalov): add support for two transport line types
-        //TODO(earalov): do we actually need to redirect this?
-        public static ushort StartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason reason, TransferManager.TransferOffer offer, string prefabName)
+        //TODO(earalov): do we need to redirect this?
+        public static void StartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason reason, TransferManager.TransferOffer offer, string prefabName)
         {
-            SimulationManager instance1 = Singleton<SimulationManager>.instance;
-            VehicleManager instance2 = Singleton<VehicleManager>.instance;
-            ushort vehicle = 0;
+            //begin mod(+): some initializing
             var buildingAi = Singleton<BuildingManager>.instance.m_buildings.m_buffer[(int)buildingID].Info?.m_buildingAI;
             var depotAi = buildingAi as DepotAI;
             if (depotAi == null)
             {
               throw new Exception("Non-depot building was selected as depot! Actual AI type: " + buildingAi?.GetType()); 
             }
+            //end mod
 
             if (reason == depotAi.m_transportInfo.m_vehicleReason)
             {
-                VehicleInfo vehicleInfo = VehicleManagerMod.GetVehicleInfo(ref instance1.m_randomizer, depotAi.m_info.m_class, offer.TransportLine, prefabName);
-                if ((Object)vehicleInfo != (Object)null)
-                {
-                    Vector3 position;
-                    Vector3 target;
-                    depotAi.CalculateSpawnPosition(buildingID, ref data, ref instance1.m_randomizer, vehicleInfo, out position, out target);
-                    if (instance2.CreateVehicle(out vehicle, ref instance1.m_randomizer, vehicleInfo, position, reason, false, true))
-                    {
-                        vehicleInfo.m_vehicleAI.SetSource(vehicle, ref instance2.m_vehicles.m_buffer[(int)vehicle], buildingID);
-                        vehicleInfo.m_vehicleAI.StartTransfer(vehicle, ref instance2.m_vehicles.m_buffer[(int)vehicle], reason, offer);
-                    }
-                }
+                //begin mod(*): use our custom vehicle manager
+                VehicleInfo randomVehicleInfo = VehicleManagerMod.GetVehicleInfo(ref Singleton<SimulationManager>.instance.m_randomizer, depotAi.m_info.m_class, offer.TransportLine, prefabName);
+                //end mod
+                if (randomVehicleInfo == null)
+                    return;
+                Array16<Vehicle> vehicles = Singleton<VehicleManager>.instance.m_vehicles;
+                Vector3 position;
+                Vector3 target;
+                depotAi.CalculateSpawnPosition(buildingID, ref data, ref Singleton<SimulationManager>.instance.m_randomizer, randomVehicleInfo, out position, out target);
+                ushort vehicle;
+                if (!Singleton<VehicleManager>.instance.CreateVehicle(out vehicle, ref Singleton<SimulationManager>.instance.m_randomizer, randomVehicleInfo, position, reason, false, true))
+                    return;
+                randomVehicleInfo.m_vehicleAI.SetSource(vehicle, ref vehicles.m_buffer[(int)vehicle], buildingID);
+                randomVehicleInfo.m_vehicleAI.StartTransfer(vehicle, ref vehicles.m_buffer[(int)vehicle], reason, offer);
             }
-            return vehicle;
+            else if (depotAi.m_secondaryTransportInfo != null && reason == depotAi.m_secondaryTransportInfo.m_vehicleReason)
+            {
+                //begin mod(*): use our custom vehicle manager
+                VehicleInfo randomVehicleInfo = VehicleManagerMod.GetVehicleInfo(ref Singleton<SimulationManager>.instance.m_randomizer, depotAi.m_info.m_class, offer.TransportLine, prefabName);
+                //end mod
+                if (randomVehicleInfo == null)
+                    return;
+                Array16<Vehicle> vehicles = Singleton<VehicleManager>.instance.m_vehicles;
+                Vector3 position;
+                Vector3 target;
+                depotAi.CalculateSpawnPosition(buildingID, ref data, ref Singleton<SimulationManager>.instance.m_randomizer, randomVehicleInfo, out position, out target);
+                ushort vehicle;
+                if (!Singleton<VehicleManager>.instance.CreateVehicle(out vehicle, ref Singleton<SimulationManager>.instance.m_randomizer, randomVehicleInfo, position, reason, false, true))
+                    return;
+                randomVehicleInfo.m_vehicleAI.SetSource(vehicle, ref vehicles.m_buffer[(int)vehicle], buildingID);
+                randomVehicleInfo.m_vehicleAI.StartTransfer(vehicle, ref vehicles.m_buffer[(int)vehicle], reason, offer);
+            }
+            //begin mod(-): no need fo this else case
+            //end mid
         }
     }
 }
