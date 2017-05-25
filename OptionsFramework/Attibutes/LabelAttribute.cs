552680349e256f5b48f6eb121ad85f8c7a135824
Egor Aralov <aei.omsk@gmail.com>
2017-05-25 20:56:34 +0200

    Update options framework

---
+++ OptionsFramework/Attibutes/LabelAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ImprovedPublicTransport2.OptionsFramework.Attibutes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class LabelAttribute : AbstractOptionsAttribute
+    {
+        public LabelAttribute(string description, string group) :
+            base(description, group, null, null)
+        {
+        }
+    }
+}
