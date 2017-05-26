c7be373abaa96b3ab295a42c3ffbc6ca40ce97b8
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2017-05-26 07:48:55 +0200

    Update version to 4.0.3

--- ImprovedPublicTransportMod.cs
+++ ImprovedPublicTransportMod.cs
@@ -14,7 +14,7 @@ namespace ImprovedPublicTransport2
     public static bool inGame = false;
     private GameObject _iptGameObject;
     private GameObject _worldInfoPanel;
-    private readonly string version = "4.0.2";
+    private readonly string version = "4.0.3";
 
     public string Name => $"Improved Public Transport 2 [r{version}]";
 
