fece386e06a85ba234610ff73c7abca3b9c054d4
Egor Aralov <aei.omsk@gmail.com>
2017-10-22 22:36:06 +0200

    Bump version

--- ImprovedPublicTransportMod.cs
+++ ImprovedPublicTransportMod.cs
@@ -14,7 +14,7 @@ namespace ImprovedPublicTransport2
     public static bool inGame = false;
     private GameObject _iptGameObject;
     private GameObject _worldInfoPanel;
-    private readonly string version = "4.0.3";
+    private readonly string version = "4.1.0";
 
     public string Name => $"Improved Public Transport 2 [r{version}]";
 
