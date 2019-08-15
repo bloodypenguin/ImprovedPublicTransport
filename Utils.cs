e874ba534638b017c2bbf5e2fcc0c8d0b787927a
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2019-08-15 21:53:02 +0200

    Add More Vehicles compatibility

--- Utils.cs
+++ Utils.cs
@@ -207,5 +207,20 @@ namespace ImprovedPublicTransport2
       }
       return (string) null;
     }
+    
+    public static bool IsModActive(ulong modId)
+    {
+      try
+      {
+        var plugins = PluginManager.instance.GetPluginsInfo();
+        return plugins.Any(p => p != null && p.isEnabled && p.publishedFileID.AsUInt64 == modId);
+      }
+      catch (Exception e)
+      {
+        UnityEngine.Debug.LogError($"Failed to detect if mod {modId} is active");
+        UnityEngine.Debug.LogException(e);
+        return false;
+      }
+    }
   }
 }
