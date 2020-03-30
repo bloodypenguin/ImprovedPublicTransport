8af484bd0b0d743ffbd9bc5c8926f0eb5f409ed2
DNKpp <DNKpp2011@gmail.com>
2020-03-30 05:53:34 +0200

    cache vehicleID from "pre" functions; accumulate passangers for each continuous vehicle
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- HarmonyPatches/LoadPassengersPatch.cs
+++ HarmonyPatches/LoadPassengersPatch.cs
@@ -1,20 +1,24 @@
 using ImprovedPublicTransport2.Detour;
+using ImprovedPublicTransport2.Util;
 
 namespace ImprovedPublicTransport2.HarmonyPatches
 {
     public class LoadPassengersPatch
     {
         private static ushort _transferSize1 = 0;
-        
-        public static bool LoadPassengersPre(ref Vehicle data)
+        private static ushort _vehicleID = 0;
+
+        public static bool LoadPassengersPre(ushort vehicleID, ref Vehicle data, ushort currentStop, ushort nextStop)
         {
-            _transferSize1 = data.m_transferSize;
+            _transferSize1 = VehicleUtil.AccumulatePassangers(vehicleID);
+            _vehicleID = vehicleID;
             return true;
         }
         
-        public static void LoadPassengersPost(ushort vehicleID, ref Vehicle data, ushort currentStop)
+        public static void LoadPassengersPost(ushort vehicleID, ref Vehicle data, ushort currentStop, ushort nextStop)
         {
-            var num2 = (ushort)(data.m_transferSize - (uint)_transferSize1);
+            var curPassangers = VehicleUtil.AccumulatePassangers(vehicleID);
+            var num2 = curPassangers - _transferSize1;
             var ticketPrice = data.Info.m_vehicleAI.GetTicketPrice(vehicleID, ref data);
             CachedVehicleData.m_cachedVehicleData[vehicleID].Add(num2, ticketPrice);
             CachedNodeData.m_cachedNodeData[currentStop].PassengersIn += num2;
--- HarmonyPatches/UnloadPassengersPatch.cs
+++ HarmonyPatches/UnloadPassengersPatch.cs
@@ -1,22 +1,26 @@
 using ImprovedPublicTransport2.Detour;
+using ImprovedPublicTransport2.Util;
 
 namespace ImprovedPublicTransport2.HarmonyPatches
 {
     public class UnloadPassengersPatch
     {
         private static ushort _transferSize1 = 0;
-        
-        public static bool UnloadPassengersPre(ref Vehicle data)
+        private static ushort _vehicleID = 0;
+
+        public static bool UnloadPassengersPre(ushort vehicleID, ref Vehicle data, ushort currentStop, ushort nextStop)
         {
-            _transferSize1 = data.m_transferSize;
+            _transferSize1 = VehicleUtil.AccumulatePassangers(vehicleID);
+            _vehicleID = vehicleID;
             return true;
         }
-        
-        public static void UnloadPassengersPost(ushort vehicleID, ref Vehicle data, ushort currentStop)
+
+        public static void UnloadPassengersPost(ushort vehicleID, ref Vehicle data, ushort currentStop, ushort nextStop)
         {
-            var num1 = (ushort)(_transferSize1 - (uint)data.m_transferSize);
-            CachedVehicleData.m_cachedVehicleData[vehicleID].LastStopGonePassengers = num1;
-            CachedVehicleData.m_cachedVehicleData[vehicleID].CurrentStop = currentStop;
+            var curPassangers = VehicleUtil.AccumulatePassangers(vehicleID);
+            var num1 = _transferSize1 - curPassangers;
+            CachedVehicleData.m_cachedVehicleData[_vehicleID].LastStopGonePassengers = num1;
+            CachedVehicleData.m_cachedVehicleData[_vehicleID].CurrentStop = currentStop;
             CachedNodeData.m_cachedNodeData[currentStop].PassengersOut += num1;
         }
     }
