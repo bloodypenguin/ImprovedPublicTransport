1e3004ed40b34c517a0ed91974c82631447d8b49
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2020-04-01 21:42:18 +0200

    Use shared state to pass load/unload arguments between prefix and postfix

--- HarmonyPatches/LoadPassengersPatch.cs
+++ HarmonyPatches/LoadPassengersPatch.cs
@@ -5,32 +5,45 @@ namespace ImprovedPublicTransport2.HarmonyPatches
 {
     public class LoadPassengersPatch
     {
-        public static bool LoadPassengersPre(ushort vehicleID, out ushort __state)
+
+        public static bool LoadPassengersPre(ushort vehicleID, ushort currentStop, out State __state)
         {
             var data = VehicleManager.instance.m_vehicles.m_buffer[vehicleID];
             if (data.m_leadingVehicle != 0)
             {
-                __state = 0;
+                __state = new State();
                 return true;
             }
 
-            __state = VehicleUtil.GetTotalPassengerCount(vehicleID, CachedVehicleData.MaxVehicleCount);
+            __state = new State
+            {
+                vehicleID = vehicleID,
+                currentStop = currentStop,
+                currentPassengers = VehicleUtil.GetTotalPassengerCount(vehicleID, CachedVehicleData.MaxVehicleCount)
+            };
             return true;
         }
 
-        public static void LoadPassengersPost(ushort vehicleID, ushort currentStop, ushort __state)
+        public static void LoadPassengersPost(State __state)
         {
-            var data = VehicleManager.instance.m_vehicles.m_buffer[vehicleID];
+            var data = VehicleManager.instance.m_vehicles.m_buffer[__state.vehicleID];
             if (data.m_leadingVehicle != 0)
             {
                 return;
             }
 
-            var currentPassengers = VehicleUtil.GetTotalPassengerCount(vehicleID, CachedVehicleData.MaxVehicleCount);
-            var newPassengers = Mathf.Max(0, currentPassengers - __state);
-            CachedVehicleData.m_cachedVehicleData[vehicleID]
-                .BoardPassengers(newPassengers, VehicleUtil.GetTicketPrice(vehicleID), currentStop);
-            CachedNodeData.m_cachedNodeData[currentStop].PassengersIn += newPassengers;
+            var currentPassengers = VehicleUtil.GetTotalPassengerCount(__state.vehicleID, CachedVehicleData.MaxVehicleCount);
+            var passengersIn = Mathf.Max(0, currentPassengers - __state.currentPassengers);
+            CachedVehicleData.m_cachedVehicleData[__state.vehicleID]
+                .BoardPassengers(passengersIn, VehicleUtil.GetTicketPrice(__state.vehicleID), __state.currentStop);
+            CachedNodeData.m_cachedNodeData[__state.currentStop].PassengersIn += passengersIn;
+        }
+        
+        public struct State
+        {
+            public ushort vehicleID;
+            public ushort currentPassengers;
+            public ushort currentStop;
         }
     }
 }
