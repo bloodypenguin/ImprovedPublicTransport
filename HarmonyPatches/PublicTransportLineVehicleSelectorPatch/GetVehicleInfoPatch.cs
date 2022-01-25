384c3035ae1430f21b95e93faf657b23e652e624
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2022-01-25 22:07:15 +0100

    Airports compatibility preview

---
+++ HarmonyPatches/PublicTransportLineVehicleSelectorPatch/GetVehicleInfoPatch.cs
@@ -0,0 +1,31 @@
+using ImprovedPublicTransport2.Util;
+
+namespace ImprovedPublicTransport2.HarmonyPatches.PublicTransportLineVehicleSelectorPatch
+{
+    public class GetVehicleInfoPatch
+    {
+        public static void Apply()
+        {
+            PatchUtil.Patch(
+                new PatchUtil.MethodDefinition(typeof(PublicTransportLineVehicleSelector),
+                    "GetVehicleInfo"),
+                new PatchUtil.MethodDefinition(typeof(GetVehicleInfoPatch),
+                    nameof(Prefix))
+            );
+        }
+
+        public static void Undo()
+        {
+            PatchUtil.Unpatch(
+                new PatchUtil.MethodDefinition(typeof(PublicTransportLineVehicleSelector),
+                    "GetVehicleInfo")
+            );
+        }
+
+        public static bool Prefix(PublicTransportLineVehicleSelector __instance)
+        {
+            __instance.component.Hide();
+            return false;
+        }
+    }
+}
