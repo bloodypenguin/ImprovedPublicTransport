46ae69bd0b779633d566e24f5c71afed26203333
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2022-01-29 02:11:43 +0100

    Bump version

--- ImprovedPublicTransportMod.cs
+++ ImprovedPublicTransportMod.cs
@@ -19,7 +19,7 @@ namespace ImprovedPublicTransport2
     public static bool inGame = false;
     private GameObject _iptGameObject;
     private GameObject _worldInfoPanel;
-    private readonly string version = "6.0.0-preview2";
+    private readonly string version = "6.0.0-preview3";
 
 
     public string Name => $"Improved Public Transport 2 [r{version}]";
